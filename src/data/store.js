0a4a05cc60f1d6b0c181154abd553276ba25e074
Frederick Johnson <fritz@Fredericks-MacBook-Pro.local>
2019-09-01 22:55:39 -0700

    added file-level comment to store && set up middleware

--- src/data/store.js
+++ src/data/store.js
@@ -1,2 +1,14 @@
+/*
+ * data/store.js
+ *
+ * Redux Store
+ * Applies Middleware then starts the store.
+ *
+ */
+
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+
+const middleware = applyMiddleware(thunk);
+
+export const store = createStore(rootReducer, middleware);
